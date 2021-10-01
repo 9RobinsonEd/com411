de293fc25081eac28304ce156a3fa9635fe50f69
Edward Robinson <5robie82@solent.ac.uk>
2021-10-01 14:45:38 +0100

    Added 4 Multi-line code

--- Basics/simple_message.py
+++ Basics/simple_message.py
@@ -1,3 +1,9 @@
 # Display message to the standard output
 print("System Failure Imminent!")
 print("System reboot has been initiated")
+print("")
+print("...Rebooting sensory system")
+print("...Rebooting output motors")
+print("Rebooting hover engine")
+print("")
+print("System online.")
