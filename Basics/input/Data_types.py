a4d43814a1103620bbc9c36520b5115d184f9ddc
Edward Robinson <5robie82@solent.ac.uk>
2021-10-01 15:33:42 +0100

    multiple questions

---
+++ Basics/input/Data_types.py
@@ -0,0 +1,18 @@
+#Displaying Data Types
+name=input("What is your name")
+print("")
+age=input("How old are you(in Years)")
+print("")
+height=float(input("How tall are you(in cm)?"))
+print("")
+Weight=float(input("How much do you weigh (in killograms)? "))
+BMI= Weight/ (height/100)**2
+
+print(f"{name} you are {age} years old and you have a bmi of {BMI}")
+
+
+
+
+
+
+
