0c9e481a550e6a140f9118df7679b89db40a6710
Edward Robinson <5robie82@solent.ac.uk>
2021-10-01 15:14:46 +0100

    Added changeable eyes

--- Basics/Output/ascii-art.py
+++ Basics/Output/ascii-art.py
@@ -1,6 +1,6 @@
 #Beep the robot
 print("\t##########")
-print("\t# -    - #")
+print("\t#        #")
 print("\t#        #")
 print("  ##############")
 print("  #            #")
@@ -8,3 +8,8 @@ print("  #            #")
 print("  #            #")
 print("  #            #")
 print("  ##############")
+
+eye=input("please enter a character for the eye")
+print(f"Beep's expression is now as follows{eye}")
+print("##########")
+print("#        #")
--- Basics/input/user_input.py
+++ Basics/input/user_input.py
@@ -1,3 +1,20 @@
 # Ask user to enter their name
 name = input("What is your name human")
 print(f"It is nice to meet you human {name}")
+
+print("\t##########")
+print("\t#        #")
+print("\t#        #")
+print("  ##############")
+print("  #            #")
+print("  #            #")
+print("  #            #")
+print("  #            #")
+print("  ##############")
+
+eye=input("please enter a character for the eye")
+print("Beep's expression is now as follows")
+print("#######")
+print(f"#{eye}   {eye}#")
+print("# --- #")
+print("#######")
