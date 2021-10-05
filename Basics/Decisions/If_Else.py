77a99f7c1f6335cee26e04b79ab9f7a9f2a7150d
Edward Robinson <edward_robinson2000@hotmail.com>
2021-10-05 15:27:52 +0100

    added Else statement

---
+++ Basics/Decisions/If_Else.py
@@ -0,0 +1,12 @@
+#User Input
+print("Please enter the activity to be performed")
+activity_type = input()
+
+#Type of activity
+if activity_type == "calculate":
+    print("performing calculations...")
+else:
+    print("Performing activity...")
+
+#final print
+print("Activity Completed!")
